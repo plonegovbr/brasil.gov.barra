0c16e36f82df6b98e073f29bb4d2b2f7b33499cb
Érico Andrei <erico@simplesconsultoria.com.br>
2013-02-27 13:40:38 -0300

    Oculta Profile de uninstall da tela de instalação do Plone

--- src/brasil/gov/barra/config.py
+++ src/brasil/gov/barra/config.py
@@ -1,3 +1,12 @@
 # -*- coding:utf-8 -*-
+from Products.CMFPlone import interfaces as plone_interfaces
+from zope.interface import implements
 
 PROJECTNAME = "brasil.gov.barra"
+
+
+class HiddenProfiles(object):
+    implements(plone_interfaces.INonInstallable)
+
+    def getNonInstallableProfiles(self):
+        return [u'brasil.gov.barra:uninstall']
