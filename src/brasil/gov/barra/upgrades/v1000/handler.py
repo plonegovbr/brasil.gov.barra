826218ce9e0f723754878cbaaf67e0de0d357610
Érico Andrei <erico@simplesconsultoria.com.br>
2013-11-19 16:06:44 -0200

    Refatora os passos de upgrade

--- src/brasil/gov/barra/upgrades/v1000/handler.py
+++ src/brasil/gov/barra/upgrades/v1000/handler.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger(PROJECTNAME)
 
 
-def from0(context):
+def stub(context):
     ''' Passo de atualizacao para versao 1000
     '''
     logger.info('Instalacao inicial')
