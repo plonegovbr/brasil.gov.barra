737d8e0a4fc3e6345824f61fe69214016372f333
Érico Andrei <erico@simplesconsultoria.com.br>
2014-03-10 17:20:19 -0300

    Remove ugrades da listagem de produtos e perfils de instalacao

--- src/brasil/gov/barra/config.py
+++ src/brasil/gov/barra/config.py
@@ -1,12 +1,28 @@
 # -*- coding:utf-8 -*-
 from Products.CMFPlone import interfaces as plone_interfaces
+from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
 from zope.interface import implements
 
 PROJECTNAME = "brasil.gov.barra"
 
 
+class HiddenProducts(object):
+    """ Oculta produtos do QuickInstaller """
+    implements(qi_interfaces.INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return [
+            'brasil.gov.barra.upgrades.v1000',
+            'brasil.gov.barra.upgrades.v1002',
+        ]
+
+
 class HiddenProfiles(object):
+    """ Oculta profiles da tela inicial de criacao do site """
     implements(plone_interfaces.INonInstallable)
 
     def getNonInstallableProfiles(self):
-        return [u'brasil.gov.barra:uninstall']
+        return [
+            'brasil.gov.barra:uninstall',
+            'brasil.gov.barra.upgrades.v1002:default'
+        ]
