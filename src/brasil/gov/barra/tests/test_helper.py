2e49cf02414883da8eb0c846983b017c1c8c4056
Érico Andrei <erico@simplesconsultoria.com.br>
2014-06-10 19:13:06 -0300

    Refatora testes para usar plone.api

--- src/brasil/gov/barra/tests/test_helper.py
+++ src/brasil/gov/barra/tests/test_helper.py
@@ -1,33 +1,42 @@
 # -*- coding: utf-8 -*-
 from brasil.gov.barra.interfaces import IBarraInstalada
 from brasil.gov.barra.testing import INTEGRATION_TESTING
+from plone import api
 from zope.interface import alsoProvides
 
 import unittest2 as unittest
 
 
 class HelperViewTest(unittest.TestCase):
-    ''' Caso de teste da Browser View BarraHelper '''
+    """ Caso de teste da Browser View BarraHelper"""
     layer = INTEGRATION_TESTING
 
     def setUp(self):
         self.portal = self.layer['portal']
-        pp = self.portal.portal_properties
+        pp = api.portal.get_tool('portal_properties')
         self.sheet = getattr(pp, 'brasil_gov', None)
         # Como nao eh um teste funcional, este objeto
         # REQUEST precisa ser anotado com o browser layer
         alsoProvides(self.portal.REQUEST, IBarraInstalada)
 
     def test_helper_view_registration(self):
-        ''' Validamos se BarraHelper esta registrada '''
-        view = self.portal.restrictedTraverse('@@barra_helper')
+        """ Validamos se BarraHelper esta registrada"""
+        view = api.content.get_view(
+            name='barra_helper',
+            context=self.portal,
+            request=self.portal.REQUEST,
+        )
         view = view.__of__(self.portal)
         self.failUnless(view)
 
     def test_helper_view_local(self):
-        ''' Uso do metodo local '''
+        """Uso do metodo local"""
         # Obtemos a Browser view
-        view = self.portal.restrictedTraverse('@@barra_helper')
+        view = api.content.get_view(
+            name='barra_helper',
+            context=self.portal,
+            request=self.portal.REQUEST,
+        )
         # Validamos que ela retorne o valor padrao para
         # o metodo remoto(configurado em profiles/default/propertiestool.xml)
         self.assertFalse(view.local())
@@ -35,7 +44,3 @@ class HelperViewTest(unittest.TestCase):
         self.sheet.local = True
         # O resultado da consulta a Browser View deve se adequar
         self.assertTrue(view.local())
-
-
-def test_suite():
-    return unittest.defaultTestLoader.loadTestsFromName(__name__)
