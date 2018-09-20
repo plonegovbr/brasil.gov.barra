e6e9419eb5133c03e1da87fc81018d8b13d320ff
hvelarde <hector.velarde@gmail.com>
2018-09-20 19:56:18 -0300

    Use new version of JS code

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,9 @@ Changelog
 2.0b1 (unreleased)
 ^^^^^^^^^^^^^^^^^^
 
+- Atualiza o código da barra à v3.0.4.
+  [hvelarde]
+
 - Remove upgrade steps da branch 1.x.
   [hvelarde]
 
--- src/brasil/gov/barra/config.py
+++ src/brasil/gov/barra/config.py
@@ -6,11 +6,8 @@ from zope.interface import implementer
 import os
 
 
-BARRA_JS_URL = 'http://barra.brasil.gov.br/barra.js'
-BARRA_JS_FILE = BARRA_JS_URL.split('/')[-1]
 BARRA_JS_STATIC_FILE_LOCATION = os.path.join(
-    os.path.dirname(__file__), 'static', BARRA_JS_FILE)
-BARRA_JS_DEFAULT_LANGUAGE = 'pt-BR'
+    os.path.dirname(__file__), 'static')
 
 PROJECTNAME = 'brasil.gov.barra'
 
