5c1091bfd992b6490d1f4ac8b301bc8b2f855b3f
hvelarde <hector.velarde@gmail.com>
2018-09-20 18:21:03 -0300

    Clean up changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.2.5.dev0'
+version = '2.0b1.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CONTRIBUTORS.rst').read() + '\n' +
@@ -15,7 +15,7 @@ setup(name='brasil.gov.barra',
       description=".gov.br: Barra de Identidade ",
       long_description=long_description,
       classifiers=[
-          'Development Status :: 5 - Production/Stable',
+          'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
           'Framework :: Plone',
           'Framework :: Plone :: 4.2',
