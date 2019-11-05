c17a8399bf9aa14c1424308d0ec4b137456e64f7
idgserpro <idg@serpro.gov.br>
2019-11-05 15:34:23 -0300

    Back to development: 3.0.12
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '3.0.11'
+version = '3.0.12.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CONTRIBUTORS.rst').read() + '\n' +
